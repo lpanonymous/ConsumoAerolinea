6a469c591469adac94c1008740b72e3506c13441
lpanonymous <lpanonymous0101@gmail.com>
2020-06-11 00:54:15 -0500

    navegacion entre paginas

--- views/mostrarvuelos.php
+++ views/mostrarvuelos.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Airline</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="../styles/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="shortcut icon" type="image/png" href="resources/icon.png">
+    <link rel="shortcut icon" type="image/png" href="../resources/icon.png">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -18,37 +18,35 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      <a class="navbar-brand" href="#">Mostrar vuelos</a>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item active">
-            <a class="nav-link" href="views/crearvuelo.php">Crear vuelo <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="crearvuelo.php">Crear vuelo <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="views/buscarvuelo.php">Buscar vuelo <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="buscarvuelo.php">Buscar vuelo <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="views/actualizarvuelo.php">Actualizar vuelo<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="actualizarvuelo.php">Actualizar vuelo<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="views/eliminarvuelo.php">Eliminar vuelo<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="eliminarvuelo.php">Eliminar vuelo<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="views/mostrarvuelos.php">Mostrar vuelos<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="crearreservacion.php">Crear reservación<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="views/crearreservacion.php">Crear reservación<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="buscarreservacion.php">Buscar reservación<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="views/buscarreservacion.php">Buscar reservación<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="actualizarreservacion.php">Actualizar reservación<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="views/actualizarreservacion.php">Actualizar reservación<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="eliminarreservacion.php">Eliminar reservacion<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="views/eliminarreservacion.php">Eliminar reservacion<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="views/mostrarreservaciones.php">Mostrar reservaciones<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="mostrarreservaciones.php">Mostrar reservaciones<span class="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>
